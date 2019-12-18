021459e792ee95136baeb07f77a9ad48ca8df647
abe moebus <kinofevil@gmail.com>
2019-12-18 00:23:27 -0500

    the beginning

---
+++ start.py
@@ -0,0 +1,7 @@
+opcode = [0, 0]
+memory = [0 for x in range(4096)]  # initializing memory array
+V = [x for x in range(16)]  # initializing cpu registers
+I = 0  # index register
+pc = 0  # program counter
+gfx = [0 for x in range(64*32)]
+print(gfx)
