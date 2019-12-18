947064fe4682f425e98e2e52683effab2eacc0a7
abeisherald <kinofevil@gmail.com>
2019-12-18 01:30:15 -0500

    more lines

--- start.py
+++ start.py
@@ -5,17 +5,24 @@ class chip8:
         global memory = [0 for x in range(4096)]  # initializing memory array
         global V = [x for x in range(16)]  # initializing cpu registers
         global I = 0  # index register
-        global pc = 0  # program counter
+        global pc = '0x200'  # program counter always starts here
         global gfx = [0 for x in range(64*32)]  # pixel screen
         global delay_timer = 0  # 60hz timer
         global sound_timer = 0  # 60hz timer
         global stack = [x for x in range(16)]
         global sp = 0
         global key = [x for x in range(16)]
+        global fontset = []
+
+        for i in range(80):    # loading fontset at location 80. ###come back
+            memory[i] = fontset[i]
+
+        with open('rom.rom') as rom:
+            for i in range(len(rom)):
+                memory[i + 512] = rom[i]
 
     def emulate_cycle(self):
-        opcode = int(
-            ((bin(int(memory[pc], 16)) + 10000000)+bin(int(memory[pc + 1], 16))), 2)
+        opcode = (memory[pc] * 10000000) + memory[pc + 1]
 
 
 class main:
